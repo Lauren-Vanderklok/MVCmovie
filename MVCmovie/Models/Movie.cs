8809069f90718a741a597d98d3518b9832c82d1d
Grandfather-Clock <great.grandmother.clock@gmail.com>
2021-02-10 12:57:24 -0500

    done?

--- MVCmovie/Models/Movie.cs
+++ MVCmovie/Models/Movie.cs
@@ -14,6 +14,7 @@ namespace MvcMovie.Models
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+
         public DateTime ReleaseDate { get; set; }
 
         [Range(1, 100)]
